82d513a0da8fc7567e2d19d9fc114a8034380e51
Alastair Marshall <ally.hannibal@gmail.com>
2017-09-18 16:37:06 +0100

    updated ada_lib with a chi-2 function

--- ada_lib.py
+++ ada_lib.py
@@ -3,4 +3,3 @@ import numpy as np
 def chi_square(data_array, model_array, error_array):
     if len(data_array) == len(model_array) == len(error_array):
         return np.sum(((data_array - model_array)/(error_array))**2)
-
