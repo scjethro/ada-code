216a8c01a93c4e696d4eac3629a0701f001edeb8
Alastair Marshall <ally.hannibal@gmail.com>
2017-09-18 17:00:09 +0100

    Random number generator

--- ada_lib.py
+++ ada_lib.py
@@ -1,5 +1,12 @@
 import numpy as np
 
-def chi_square(data_array, model_array, error_array):
-    if len(data_array) == len(model_array) == len(error_array):
-        return np.sum(((data_array - model_array)/(error_array))**2)
+def func_rand_uniform(a, b, i_seed):
+    """
+    :param A: lower limit of uniform distribution
+    :param B: upper limit of uniform distribution
+    :param i_seed: seed integer that is used
+    :return: random number from the uniform distribution
+    """
+
+    np.random.seed(i_seed)
+    return (b-a)*np.random.random() + a
